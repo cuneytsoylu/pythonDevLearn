8d49e7a7660e334d4b753789401108e0c135b3fc
cuneytsoylu <110092795+cuneytsoylu@users.noreply.github.com>
2023-06-23 23:14:26 +0300

    python

---
+++ python/class/my_package/my_math.py
@@ -0,0 +1,31 @@
+def factorial(x):
+    """Bu fonksiyon içerisine yazilan sayinin faktöriyel değerini bulur"""
+
+    assert x >= 0, "Negatif sayilarin faktöriyeli aranmaz"
+
+    carpim_degeri = 1
+    for i in range(x, 0, -1):
+
+        carpim_degeri *= i
+
+    return carpim_degeri
+
+
+def my_pow(x, y):
+
+    return x ** y
+
+
+def mean(x, y):
+    """Bu fonksiyon iki sayının ortalamasını verir."""
+
+    return (x + y) / 2
+
+
+def mean_(* x):
+    """Girdiğiniz sayıların ortalamasını döndürür."""
+
+    return sum([i for i in x]) / len(x)
+
+
+my_pi = 3.14
